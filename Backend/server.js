bafcc98e5a26518c1824bbc8fcf193aa53a8a9a8
Kaushik Mudaliyar <kaushikmudaliyar1484@gmail.com>
2026-05-06 16:31:59 +0530

    bug fixes in server file

--- Backend/server.js
+++ Backend/server.js
@@ -3,8 +3,8 @@ import app from "./src/app.js";
 import { connectDb } from "./src/db/db.js";
 import { conf } from "./src/conf/conf.js";
 
-const port = conf.PORT || 8000;
+const port = conf.PORT || 3000;
 connectDb();
 app.listen(port, () => {
-  console.log("Server is listening on port 3000");
+  console.log(`Server is listening on port ${port}`);
 });
